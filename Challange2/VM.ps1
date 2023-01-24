2904f551918df0a0062804df12af60e4a1ed9b0d
ShubhamTiwari <v-epam-shubham.tiwari@metrolinx.com>
2023-01-24 20:09:57 +0530

    adding PS script for Challange 2

---
+++ Challange2/VM.ps1
@@ -0,0 +1,23 @@
+﻿$result = @()
+$VMs = get-azVm
+foreach ($VM in $Vms)
+{
+  $NICs = Get-AzNetworkInterface -ResourceId $Vm.NetworkProfile.NetworkInterfaces.id
+
+  foreach ($NIC in $NICs){
+   $ipconfig = Get-AzNetworkInterfaceIpConfig -NetworkInterface $NIC  
+
+  }
+    $Status = Get-AzVM -Name $vm.Name -Status 
+    $Output = New-Object -TypeName psobject 
+    $Output | Add-Member NoteProperty -Name VMName -Value $Vm.Name
+    $Output | Add-Member NoteProperty -Name VMLocation -value $VM.Location
+    $Output | Add-Member NoteProperty -Name PrivateIp -value $ipconfig.PrivateIpAddress
+    $Output | Add-Member NoteProperty -Name PrivateIpAllocation -value $ipconfig.PrivateIpAllocationMethod
+    $Output | Add-Member NoteProperty -Name OSType -Value $Vm.StorageProfile.ImageReference.Offer
+    $Output | Add-Member NoteProperty -Name OSName -Value $Vm.StorageProfile.ImageReference.Sku
+    $Output | Add-Member NoteProperty -Name VMStatus -Value $status.PowerState 
+    
+    $result += $Output
+}
+$result | ConvertTo-Json | Out-File C:\result.json -Encoding ascii
